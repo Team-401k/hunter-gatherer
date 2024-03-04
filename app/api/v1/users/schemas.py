7dfa57f11c7e6d4fa63887f5547726ad391ab04c
Saihaj Gulati <ssgulati@usc.edu>
2024-03-04 00:09:05 -0800

    parsed paypal stuff

--- app/api/v1/users/schemas.py
+++ app/api/v1/users/schemas.py
@@ -1,6 +1,7 @@
 """Schemas for users."""
 
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Stub(BaseModel):
@@ -8,3 +9,19 @@ class Stub(BaseModel):
 
     some_string: str
 
+
+class TransactionAmount(BaseModel):
+    currency_code: str
+    value: str
+
+class TransactionInfo(BaseModel):
+    transaction_id: str
+    transaction_event_code: str
+    transaction_initiation_date: str
+    transaction_updated_date: str
+    transaction_amount: TransactionAmount
+    transaction_status: str
+    bank_reference_id: Optional[str] = None
+    ending_balance: TransactionAmount
+    available_balance: TransactionAmount
+    protection_eligibility: str

63ab224bda776d2a30f10e7a5a89db78affd27b9
Aadesh Bajaj <aadeshba@usc.edu>
2024-03-03 22:59:41 -0800

    users object made

--- app/api/v1/users/models.py
+++ app/api/v1/users/models.py
@@ -1,6 +1,6 @@
 
 
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, Date
 from app.database import Base, current_utc_time
 
 
@@ -10,6 +10,17 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, nullable=False)
     email=Column(String, nullable=False)
+    status = Column(String)
+    types = Column(String)
+    address = Column(String)
+    phone = Column(String)
+    usps = Column(String)
+    date_joined = Column(Date)
+    date_renewed = Column(Date)
+    emergency_contact = Column(String)
+    emergency_phone = Column(String)
+    is_member = Column(Boolean)
+    date_expired = Column(Date)
 
 
     def __repr__(self):

2a01deb7b55f1a4ca4a3f4d1ec79d9ec054cac07
Saihaj Gulati <ssgulati@usc.edu>
2024-03-29 03:53:41 -0700

    Update fillingdb.py

--- app/api/v1/fillingdb.py
+++ app/api/v1/fillingdb.py
@@ -3,6 +3,8 @@ from app.api.v1.external_apis.stripe_api import StripeAPI
 from app.api.v1.external_apis.sqsp_api import SquareSpaceAPI
 
 #would call this function with dates to get all three for that month
+#notice how for each API, only have to create the object and then call the search_parse function
+#also, all now take the same date format, so this fill_db function's parameters can just be passed to each api's seach_parse function to get the orders from that month
 def fill_db(start_date: str, end_date: str):
     # First get the parsed orders from paypal
     paypal_api = PayPalAPI()
@@ -34,5 +36,5 @@ def fill_db(start_date: str, end_date: str):
     for order in sqsp_orders:
         print(order.grandTotal.value)
         
-
+#currently doesn't fill the db actually, but does call all three apis for orders within the dates passed, parses their input, and then prints the amounts of each order 
 fill_db('2024-02-16T00:00:00Z', '2024-03-10T23:59:59Z')

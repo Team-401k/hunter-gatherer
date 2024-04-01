fb1f5fc36e777b2699be1b30ee23d3a05515783e
Aditya Hariharan <adityaha@usc.edu>
2024-04-01 01:28:51 -0700

    pushing controller

--- app/api/v1/orders/controllers.py
+++ app/api/v1/orders/controllers.py
@@ -16,6 +16,69 @@ from app.api.v1.external_apis.sqsp_api import SquareSpaceAPI
 
 router = APIRouter()
 
+@router.post("/sqsp_ingestion")
+def ingest_sqsp_orders(session: Session = Depends(db)):
+    sqsp_transactions: SqspTransactionsResponse = services.get_transactions('2024-02-16T00:00:00Z', '2024-03-10T23:59:59Z')
+    for transaction in sqsp_transactions.documents:
+        # create order
+        new_order = Order(
+            purchase_id=transaction.salesOrderId,
+            user_ids=[], # need to add user
+            amount=transaction.total.value,
+            date = transaction.createdOn,
+            # add sku from line items later
+            payment_method=transaction.payments[0].provider,
+            fee=transaction.total.value - transaction.totalNetPayment.value,
+            external_transaction_id=transaction.payments[0].externalTransactionId,
+        )
+
+        # if transaction has salesOrderId then it is a product order
+        # otherwise donation
+        if not transaction.salesOrderId:
+        # create donation
+            pass
+        else:
+            # create an order + user (if needed) from order details
+            order_detail = services.get_order_detail(transaction.salesOrderId)
+            for line_item in order_detail.lineItems:
+                if 'forum' in line_item.productName.lower():
+                    first_name, last_name, phone, email = services.parse_forum_customizations(line_item.customizations)
+    
+                    name = first_name + " " + last_name
+                    address = order_detail.billingAddress.address1 + " " + order_detail.billingAddress.address2 + ", " + order_detail.billingAddress.city + ", " + order_detail.billingAddress.state + " " + order_detail.billingAddress.postalCode
+
+                    user = get_user_by_email(email)
+                    if not user:
+                        user = User(
+                            email=email,
+                            name=name,
+                            address=address,
+                            phone=phone,
+                        )
+
+                        session.add(user)
+                        session.flush()
+                        session.refresh(user)
+
+                    new_order.user_ids.append(new_user.id)
+                elif 'membership' in line_item.productName.lower():
+                    address_line, city, state, postal_code, phone, name, email, emergency_contact, emergency_contact_phone = services.parse_membership_customizations(line_item.customizations)
+
+                    address = address_line + ", " + city + ", " + state + " " + postal_code
+                    # need to add check for if user exists or not
+                    new_user = User(
+                        email=email,
+                        name=name,
+                        address=address,
+                        phone=phone,
+                        emergency_contact=emergency_contact,
+                        emergency_contact_phone=emergency_contact_phone,
+                    )
+
+                    session.add(new_user)
+                    session.flush()                 
+                    new_order.user_ids.append(new_user.id)
+
 
 
 @router.post("/sqsp_ingestion")

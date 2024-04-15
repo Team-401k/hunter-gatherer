b05fc347a6deb58592371ae3dcfe7819699006f8
Aadesh Bajaj <aadeshba@usc.edu>
2024-04-14 23:16:13 -0700

    reverted earlier mini commit (too lazy to actulaly revert commit

--- __main__.py
+++ __main__.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
 import uvicorn
-import threading
-import sys
-
-def exit_application():
-    sys.exit(0)
 
 def main():
-
-    timeout = 50
-    
-    # Start the timer
-    timer = threading.Timer(timeout, exit_application)
-    timer.start()
-
     """Entrypoint of the application"""
     uvicorn.run(
         "app.app:get_app",
@@ -21,7 +9,6 @@ def main():
         factory=True,
         timeout_keep_alive=60,
     )
-    exit()
 
 if __name__ == "__main__":
     main()
